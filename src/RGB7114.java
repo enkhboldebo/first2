c8ae8692a3fff2ff471ce9dcaacbd0c623e931a9
Erdene <ebo2006e@gmail.com>
2021-09-19 10:24:46 -0500

    a

---
+++ src/RGB7114.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class RGB7114 {
+
+    public static void main(String[] args){
+        int a, b, c;
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+        c = input.nextInt();
+        if( a%2 == 0 ) {
+            System.out.println("YES");
+        }else{
+            System.out.println( "NO" );
+        }if( b%2 == 0 ) {
+            System.out.println("YES");
+        }else{
+            System.out.println( "NO" );
+        }if( c%2 == 0 ){
+            System.out.println( "YES" );
+        }else{
+            System.out.println( "NO" );
+        }
+    }
+}
