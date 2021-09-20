d5135db25ab742e0ef1077efd2c1928f9360563b
Erdene <ebo2006e@gmail.com>
2021-09-20 17:06:48 -0500

    a

---
+++ src/RGB7201.java
@@ -0,0 +1,6 @@
+public class RGB7201 {
+
+    public static void main(String[] args){
+        System.out.println( "IOI" );
+    }
+}
---
+++ src/RGB7202.java
@@ -0,0 +1,8 @@
+public class RGB7202 {
+
+    public static void main(String[] args){
+        System.out.println( "IOI");
+        System.out.println( "IOI");
+        System.out.println( "IOI");
+    }
+}
